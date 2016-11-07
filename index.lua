9e522fc535cf75fd39196c2d46823403a5e3cfc7
Ale32bit <ale.proto00@gmail.com>
2016-11-07 23:38:31 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,5 +1,7 @@
 {
-  ["BeaconNet/example.spk"] = "SPK Example",
+  
   ["Ale32bit/bluecloud.spk"] = "BlueCloud",
   ["Ale32bit/beam.spk"] = "Beam",
+  ["QuantumTech/qpay.spk"] = "QPay",
+  ["BeaconNet/example.spk"] = "SPK Example",
 }
